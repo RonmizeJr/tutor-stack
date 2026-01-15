f93995feb1d6b004b2a9645f424ae2162c8ac72b
Ronald Mize Jr <rwthojr5@gmail.com>
2026-01-15 01:38:16 -0600

    more config changes

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,4 +1,4 @@
-const clerkIssuerUrl = process.env.CLERK_DOMAIN_ISSUER!;
+const clerkIssuerUrl = process.env.CLERK_DOMAIN_ISSUER;
 
 if (!clerkIssuerUrl) {
   throw new Error('Missing CLERK_ISSUER_URL environment variable');
